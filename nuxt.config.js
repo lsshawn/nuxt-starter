8fe5ddab7d7e099dd5d7302d0e4cce13b518c4ce
Shawn <coffee.or.tea88@gmail.com>
2018-07-11 01:14:16 +0800

    change git for netlify cms

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   head: {
-    title: 'Alid Castano',
+    title: 'Nuxt CMS Starter',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
